135d907c17a2101dba278213a13ab73d72319e65
KrisGol <kirogol@yandex.ru>
2021-09-28 09:24:50 +0600

    создание сайта начало, главная

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie-edge">
+  <script type="text/javascript" src="share42\share42.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>сайт</title>
+</head>
+<body class="body">
+  <header class="header-style">
+    <ul class="menu-main">
+      <li><a href="#">Главная</a></li>
+      <li><a href="#" >Породы</a></li>
+      <li><a href="#" >Питомник</a></li>
+      <li><a href="#" >О клубе</a></li>
+    </ul>
+  </header>
+      <div id="wrapper" class="wrapper">
+        <div id="section" class="section-1">
+            <div id="container" class="container">
+              <div id="head" class="head-text">
+                <h1 >я люблю собак</h1>
+                <p >Клуб любителей собак "Дружок"</p>
+              </div>
+              <div id="social network" class="social-network">
+                <div class="text-social" >Мы в социальных сетях</div>
+                  <div id="link-block" class="link-block">
+                <div class="share42init"></div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        <div id="section2" class="section-2">
+          <div id="container" class="container">
+            <div id="block-care" class="block-care">
+                <div id="text-care" class="text-care">
+                      <div>Маленькое чудо</div>
+                      <li><a href="#" >Дома появился маленький щенок. Правила ухода, кормление, гигиена</a></li>
+                </div>
+            </div>
+              <div id="foto-img-1" class="foto-img-1"></div>
+          </div>
+        </div>
+        <div id="section3" class="section-3">
+          <div id="container" class="container">
+
+            <div id="info-text" class="info-text">
+                      <div>Владеть собакой - очень итересно</div>
+                </div>
+                <div id="foto-img-2" class="foto-img-2"></div>
+                    <div id="block-1" class="block-1">
+                      <div id="text-competition" class="text-competition"> Участие в выставках
+                      <li><a href="#" >Подготовка, описание, титулы</a></li>
+                      </div>
+                      </div>
+                    <div id="block-2" class="block-2">
+                      <div id="text-competition" class="text-competition"> Участие в выставках
+                      <li><a href="#" >Подготовка, описание, титулы</a></li>
+                      </div>
+                    </div>
+          </div>
+        </div>
+    </div>
+
+  <footer class="footer"> </footer>
+</body>
+</html>
